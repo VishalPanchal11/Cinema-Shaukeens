8d59e41183925fdca97942d8264585a512bdc6ce
Vishal Panchal <115503642+VishalPanchal11@users.noreply.github.com>
2024-04-12 02:23:19 +0530

    API fetched and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg"
+const apiURL = "https://www.omdbapi.com?apikey=c5beb882";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`https://www.omdbapi.com?apikey=c5beb882&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("batman");
+  }, []);
+
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div className="app">
+      <h1>Cinema Shaukeens</h1>
     </div>
   );
 };
