8009eabae5de56abb34c2848dfd1cd67ff5ebbf1
Vishal Panchal <115503642+VishalPanchal11@users.noreply.github.com>
2024-04-12 03:40:02 +0530

    adding search bar functionality

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ const MovieCard = ({ movie }) => {
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm]=useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(
@@ -57,10 +58,10 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for Movies"
-          value="Batman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
       {movies.length > 0 ? (
         <div className="container">
